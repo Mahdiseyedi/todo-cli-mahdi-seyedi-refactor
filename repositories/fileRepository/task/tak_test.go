f8840202775c7918260babdd2c229e5369e715e3
Mahdi_seyedi <mahdisayyedi19@gmail.com>
2023-06-28 12:50:52 +0330

    task done

--- repositories/fileRepository/task/tak_test.go
+++ repositories/fileRepository/task/tak_test.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -157,5 +158,68 @@ func TestTaskTextDeserializer(t *testing.T) {
 	err = os.Remove("test.txt")
 	if err != nil {
 		t.Errorf("can't delete test file: %v", err)
+	}
+}
+
+func TestTaskDeserializer(t *testing.T) {
+
+	fs := FileStore{serializationMode: consts.TextSerializationMode}
+	pData := []string{
+		"id: 1, title: Buy groceries, dueDate: 2021-12-31, categoryID: 2, isDone: false, userID: 3",
+		"id: 2, title: Clean the house, dueDate: 2022-01-01, categoryID: 1, isDone: true, userID: 4",
+		"id: 3, title: Read a book, dueDate: 2022-01-02, categoryID: 3, isDone: false, userID: 5",
+		"id: 4, title: Watch a movie, dueDate: 2022-01-03, categoryID: 4, isDone: true, userID: 6",
+		"id: invalid, title: Do nothing, dueDate: 2022-01-04, categoryID: 5, isDone: false, userID: 7",
+	}
+
+	tasks := fs.TaskDeserializer(pData)
+	if len(tasks) != 4 {
+		t.Errorf("expected 4 tasks, got %d", len(tasks))
+	}
+
+	expectedTasks := []models.Task{
+		{ID: 1, Title: "Buy groceries", DueDate: "2021-12-31", CategoryID: 2, IsDone: false, UserID: 3},
+		{ID: 2, Title: "Clean the house", DueDate: "2022-01-01", CategoryID: 1, IsDone: true, UserID: 4},
+		{ID: 3, Title: "Read a book", DueDate: "2022-01-02", CategoryID: 3, IsDone: false, UserID: 5},
+		{ID: 4, Title: "Watch a movie", DueDate: "2022-01-03", CategoryID: 4, IsDone: true, UserID: 6},
+	}
+
+	for i, task := range tasks {
+		if task != expectedTasks[i] {
+			t.Errorf("expected task %v, got %v", expectedTasks[i], task)
+		}
+	}
+}
+
+func TestSave(t *testing.T) {
+
+	tmpfile, err := ioutil.TempFile("", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+
+	fs := FileStore{Filepath: tmpfile.Name(), serializationMode: consts.TextSerializationMode}
+	task := models.Task{ID: 1, Title: "Buy groceries", DueDate: "2021-12-31", CategoryID: 2, IsDone: false, UserID: 3}
+
+	fs.Save(task)
+
+	tmpfile.Close()
+
+	file, err := os.Open(tmpfile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var line string
+	for scanner.Scan() {
+		line = scanner.Text()
+	}
+
+	expectedLine := "id: 1, title: Buy groceries, dueDate: 2021-12-31, categoryID: 2, isDone: false, userID: 3"
+	if line != expectedLine {
+		t.Errorf("expected line %s, got %s", expectedLine, line)
 	}
 }
